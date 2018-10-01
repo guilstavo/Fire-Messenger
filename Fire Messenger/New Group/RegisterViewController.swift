26e00187525bb0094916f14208a534770b746776
gustavosysm <gustavo.santos@systemsmobile.com.br>
2018-10-01 17:41:45 +0100

    Initial Commit

---
+++ Fire Messenger/New Group/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  Fire Messenger
+//
+//  Created by Gustavo M Santos on 01/10/2018.
+//  Copyright © 2018 Gustavo M Santos. All rights reserved.
+//
+
+import Foundation
